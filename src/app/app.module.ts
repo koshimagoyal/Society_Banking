6590aeb36ce4223ef2f3264ba48147ae992591da
Koshima Goyal <koshimagoyal@yahoo.co.in>
2020-05-21 20:39:05 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
 }
